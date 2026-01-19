4f57f47cd44fc45f25c1c09f31878e98de195554
Orlando Pierro <opierro25@uwcchina.org>
2026-01-19 09:51:26 +0800

    Create multiple_inheritance.py

---
+++ multiple_inheritance.py
@@ -0,0 +1,5 @@
+# multiple inheritnace = inherit from more than one parent class
+#                        C(A, B)
+
+# multilevel inheritance = inherit from a parent which inherits from another parent 
+#                          C(B) <- B(A) <- A
